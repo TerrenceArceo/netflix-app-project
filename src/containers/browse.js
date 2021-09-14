fabb63ab23fc9a635914e00394e1f2a0b81a31b2
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2021-09-13 19:03:34 -0700

    debugging the deployment of the site

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -86,7 +86,7 @@ export function BrowseContainer({ slides }) {
                         Watch Joker Now
                     </Header.FeatureCallOut>
                     <Header.Text>
-                        Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a futile attempt to feel like he's part of the world around him.
+                        Forever alone in a Crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a futile attempt to feel like he's part of the world around him.
                     </Header.Text>
                     <Header.PlayButton>Play</Header.PlayButton>
                 </Header.Feature>
